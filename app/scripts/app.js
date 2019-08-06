54b975f6ca16c71f8576d38ee49da04035bc4d68
7belo <7belo@tutanota.com>
2019-08-06 11:43:30 +0200

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name deslocamentosApp
+ * @description
+ * # deslocamentosApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('deslocamentosApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deslocamentosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deslocamentosApp
+ */
+angular.module('deslocamentosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
